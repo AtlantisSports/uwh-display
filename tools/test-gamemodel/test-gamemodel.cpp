316baf4e8d9bb5cef1b5370eebce0512a176899f
Jonathan Roelofs <jonathan@codesourcery.com>
2016-01-09 00:59:35 +0000

    Add a few more GameModel serialization tests

--- tools/test-gamemodel/test-gamemodel.cpp
+++ tools/test-gamemodel/test-gamemodel.cpp
@@ -13,7 +13,7 @@
 
 using namespace uwhtimer;
 
-bool TestSerialization(unsigned B, unsigned W, unsigned T) {
+bool CheckRoundTripSerialize(unsigned B, unsigned W, unsigned T) {
   GameModel Src;
   Src.BlackScore = B;
   Src.WhiteScore = W;
@@ -37,13 +37,77 @@ bool TestSerialization(unsigned B, unsigned W, unsigned T) {
   return false;
 }
 
+bool CheckNotEquals(GameModel M1, GameModel M2) {
+  if (M1 == M2) {
+    std::cerr << "FAIL: " << M1.serialize() << " == " << M2.serialize() << " "
+              << __PRETTY_FUNCTION__ << " operator==\n";
+    return true;
+  }
+
+  if (!(M1 != M2)) {
+    std::cerr << "FAIL: " << M1.serialize() << " == " << M2.serialize() << " "
+              << __PRETTY_FUNCTION__ << " operator!=\n";
+    return true;
+  }
+
+  if (M1.BlackScore == M2.BlackScore &&
+      M1.WhiteScore == M2.WhiteScore &&
+      M1.GameClockSecs == M2.GameClockSecs) {
+    std::cerr << "FAIL: " << M1.serialize() << " == " << M2.serialize() << " "
+              << __PRETTY_FUNCTION__ << " equality\n";
+    return true;
+  }
+
+  return false;
+}
+
+bool CheckEquals(GameModel M1, GameModel M2) {
+  if (M1 != M2) {
+    std::cerr << "FAIL: " << M1.serialize() << " != " << M2.serialize() << " "
+              << __PRETTY_FUNCTION__ << " operator!=\n";
+    return true;
+  }
+
+  if (!(M1 == M2)) {
+    std::cerr << "FAIL: " << M1.serialize() << " != " << M2.serialize() << " "
+              << __PRETTY_FUNCTION__ << " operator==\n";
+    return true;
+  }
+
+  if (M1.BlackScore != M2.BlackScore ||
+      M1.WhiteScore != M2.WhiteScore ||
+      M1.GameClockSecs != M2.GameClockSecs) {
+    std::cerr << "FAIL: " << M1.serialize() << " != " << M2.serialize() << " "
+              << __PRETTY_FUNCTION__ << " inequality\n";
+    return true;
+  }
+
+  return false;
+}
+
+
 int main(int argc, char *argv[]) {
   bool Failed = false;
 
   for (unsigned B = 0; B < 10; B++)
     for (unsigned W = 0; W < 10; W++)
       for (unsigned T = 0; T < 10; T++)
-        Failed != TestSerialization(B, W, T);
+        Failed != CheckRoundTripSerialize(B, W, T);
+
+  Failed != CheckRoundTripSerialize(99, 99, 9999);
+  Failed != CheckRoundTripSerialize(99, 99, 999999);
+  Failed != CheckRoundTripSerialize(0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF);
+
+  GameModel M1;
+  M1.BlackScore = 1;
+  M1.WhiteScore = 2;
+  M1.GameClockSecs = 15;
+
+  GameModel M2 = M1;
+  Failed != CheckEquals(M1, M2);
+
+  M1.BlackScore = 2;
+  Failed != CheckNotEquals(M1, M2);
 
   return Failed ? 1 : 0;
 }
