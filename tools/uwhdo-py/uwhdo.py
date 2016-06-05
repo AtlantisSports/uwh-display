60710e58f91ca7d72166f2cbe9d293c3e41a205a
eyrardurgur <orri.jonsson@gmail.com>
2016-06-04 20:50:13 -0600

    [uwhdo] Cleaned up layout. Added border.
    The grid now works and has enough border around it that I can create a
    transparency layer for it to look decent.

--- tools/uwhdo-py/uwhdo.py
+++ tools/uwhdo-py/uwhdo.py
@@ -48,35 +48,24 @@ class Example(Frame):
         self.parent.title("TimeShark Scores")
         self.pack(fill=BOTH, expand=1)
 
-        #Creating Variables
-        # scoreAlign_x    = 80
-        # teamAlign_x     = 20
-        # timeAlign_x     = 30
-        # whiteAlign_y    = 20
-        # blackAlign_y    = 45
-        # timeAlign_y     = 70
-        # labelFont       = "Purisa"
-        # whiteColor      = "#ffffff"
-        # blackColor      = "#0000ff"
-        # timeColor       = "#00ff00"
-        # boxMargin       = 5
-        # boxWidth        = 150
-        # boxHeight       = 75
-        # boxOutline1     = "#222222"
-        # boxColor1       = "#000000"
-        # fontSize        = 20
-
-        score_font = ("Verdana", 12, "bold")
+        #Creating layout variables
+        ###########################################################################
+        #Fonts
+        score_font = ("Verdana", 20, "bold")
         time_font = score_font
 
-        score_width  = 20
-        score_height = 25
-        label_width = 3* score_width
+        #Element sizes
+        score_width  = 48
+        score_height = 40
+        label_width = 2.5 * score_width
         label_height = score_height
-        clock_height = 1.1 * score_height
+        clock_height = 1 * score_height
         clock_width = score_width + label_width
+        border_side_width = score_width
+        border_top_width = clock_width + 2 * border_side_width
+        border_side_height = 3 * score_height
 
-        #Setting Colors
+        #Setting colors
         white_bg = "black"
         white_fg = "white"
         black_bg = white_bg
@@ -91,8 +80,9 @@ class Example(Frame):
         self.black_score  = 4
         self.white_score = 10
         self.game_clock_time = 609
-        self.white_team = "White"
-        self.black_team = "Black"
+        self.white_team = " White"
+        self.black_team = " Black"
+        self.border_text = ""
 
         self.white_score_var = StringVar()
         self.white_score_var.set("%d" % (self.white_score,))
@@ -104,6 +94,8 @@ class Example(Frame):
         self.black_team_var.set("%s" % (self.black_team,))
         self.black_score_var = StringVar()
         self.black_score_var.set("%d" % (self.black_score,))
+        self.border_text_var = StringVar()
+        self.border_text_var.set("%s" % (self.border_text,))
 
         w = Canvas(self)
         w.pack(fill=BOTH, expand=1)
@@ -111,23 +103,42 @@ class Example(Frame):
         #w.create_rectangle(boxMargin+2*boxWidth, boxMargin, boxMargin+boxWidth, boxMargin+boxHeight, fill=boxColor1)
         #w.create_rectangle(teamAlign_x - boxMargin, whiteAlign_y - boxMargin, scoreAlign_x - 2 * boxMargin, whiteAlign_y + boxMargin, outline=boxOutline1)
 
-        #Creating a small row and column that give a small margin
+        #Table layout for time and score.
 
-        white_team = SizedLabel(w, self.white_team_var, white_bg,
-            white_fg, score_font, score_height, score_width, E)
-        white_team.grid(row=1, column=0)
-        white_score_label = SizedLabel(w, self.white_score_var, white_bg,
-            white_fg, score_font, score_height, score_width, E)
-        white_score_label.grid(row=1, column=1, ipadx=5)
-        black_team = SizedLabel(w, self.black_team_var, black_bg,
-                black_fg, score_font, score_height, score_width, E)
-        black_team.grid(row=2, column=0)
-        black_score_label = SizedLabel(w, self.black_score_var, black_bg,
-            black_fg, score_font, score_height, score_width, E)
-        black_score_label.grid(row=2, column=1, ipadx=5)
+        #Border around table
+        border_top_label = SizedLabel(w, self.border_text_var, score_bg,
+            score_fg, time_font, clock_height, border_top_width, CENTER)
+        border_bottom_label = SizedLabel(w, self.border_text_var, score_bg,
+            score_fg, time_font, clock_height, border_top_width, CENTER)
+        border_left_label = SizedLabel(w, self.border_text_var, score_bg,
+            score_fg, time_font, border_side_height, border_side_width, CENTER)
+        border_right_label = SizedLabel(w, self.border_text_var, score_bg,
+            score_fg, time_font, border_side_height, border_side_width, CENTER)
+        border_top_label.grid(row=0, column=0, columnspan=4)
+        border_bottom_label.grid(row=10, column=0, columnspan=4)
+        border_left_label.grid(row=1, column=0, rowspan=3)
+        border_right_label.grid(row=1, column=3, rowspan=3)
+
+
         game_clock_label = SizedLabel(w, self.game_clock_var, score_bg,
             score_fg, time_font, clock_height, clock_width, CENTER)
-        game_clock_label.grid(row=0, column=0, columnspan=2)
+        game_clock_label.grid(row=1, column=1, columnspan=2)
+
+        white_team = SizedLabel(w, self.white_team_var, white_bg,
+            white_fg, score_font, score_height, label_width, W)
+        white_team.grid(row=2, column=1)
+        white_score_label = SizedLabel(w, self.white_score_var, white_bg,
+            white_fg, score_font, score_height, score_width, E)
+        white_score_label.grid(row=2, column=2)
+
+        black_team = SizedLabel(w, self.black_team_var, black_bg,
+                black_fg, score_font, score_height, label_width, W)
+        black_team.grid(row=3, column=1)
+        black_score_label = SizedLabel(w, self.black_score_var, black_bg,
+            black_fg, score_font, score_height, score_width, E)
+        black_score_label.grid(row=3, column=2)
+
+
 
 def main():
 
@@ -136,6 +147,7 @@ def main():
     # make it cover the entire screen
     w, h = root.winfo_screenwidth(), root.winfo_screenheight()
     root.geometry("%dx%d-0+0" % (w, h))
+    root.attributes('-fullscreen', True)
     root.mainloop()
 
 if __name__ == '__main__':
