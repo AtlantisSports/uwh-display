fbed06c57957dcd81b492adeeb4c2309d077cc34
Jonathan Roelofs <jonathan@codesourcery.com>
2016-10-29 20:21:34 -0600

    [sync] Add stubs so that all the configurations still build

---
+++ lib/sync/NoSocketSyncServer.cpp
@@ -0,0 +1,14 @@
+//===-- NoSocketSyncServer.cpp - Socket Syncing stub --------------- c++ --===//
+//
+//                               UWH Timer
+//
+//           This file is distributed under the BSD 3-Clause License.
+//                      See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "uwhd/sync/ModelSync.h"
+
+ModelSync *CreateSocketServer(const std::string &Port) {
+  return nullptr;
+}
---
+++ lib/sync/NoXBeeSync.cpp
@@ -0,0 +1,20 @@
+//===-- NoXBeeSync.cpp - XBee Syncing stubs ------------------------ c++ --===//
+//
+//                               UWH Timer
+//
+//           This file is distributed under the BSD 3-Clause License.
+//                      See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "uwhd/sync/ModelSync.h"
+
+
+ModelSyncServer *CreateXBeeSyncServer() {
+  return nullptr;
+}
+
+ModelSync *CreateXBeeSyncClient() {
+  return nullptr;
+}
+
