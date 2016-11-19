5f39c354e6bb5bd32674fd379fe033813b8faeac
Jonathan Roelofs <jonathan@codesourcery.com>
2016-11-19 11:04:59 -0700

    [tests] Clean up the SWIG test. Add comments. NFC

--- test/swig/canvas.py
+++ test/swig/canvas.py
@@ -8,24 +8,25 @@ def main():
   white_score = 5
   time = 42
 
+  # Build a GameModelManager with a known state:
   mgr = uwhd.GameModelManager()
-
-  print('# SET-VERSION: %d' % (version,))
-  print('# SET-STATE: FirstHalf')
-  print('# SET-BLACK: %d' % (black_score,))
-  print('# SET-WHITE: %d' % (white_score,))
-  print('# SET-TIME: %d' % (time,))
-
   mgr.setGameStateFirstHalf()
   mgr.setBlackScore(black_score)
   mgr.setWhiteScore(white_score)
   mgr.setGameClock(time)
 
+  # Render that state to a canvas:
   canvas = uwhd.UWHDCanvas.create(32 * 3, 32)
-  uwhd.renderGameDisplay(1, mgr.getModel(), canvas)
-  ppmstr = uwhd.asPPMString(canvas)
+  uwhd.renderGameDisplay(version, mgr.getModel(), canvas)
 
-  print(ppmstr)
+  # Print out the expected and actual display state
+  # for display-check to verify:
+  print('# SET-VERSION: %d' % (version,))
+  print('# SET-STATE: FirstHalf')
+  print('# SET-BLACK: %d' % (black_score,))
+  print('# SET-WHITE: %d' % (white_score,))
+  print('# SET-TIME: %d' % (time,))
+  print(uwhd.asPPMString(canvas))
 
 if __name__ == '__main__':
   main()
