726b20dc1eb021f84b60eeee2c18f8071cf8d86e
Jonathan Roelofs <jonathan@codesourcery.com>
2017-01-22 09:52:18 -0700

    [scripts] Add Python3.4 to the scripts' apt-get

--- scripts/uwh-display-setup-1.sh
+++ scripts/uwh-display-setup-1.sh
@@ -7,7 +7,7 @@ fi
 
 this_script=`basename "$0"`
 
-apt-get install git cmake vim swig i2c-tools libi2c-dev ntpdate minicom --assume-yes
+apt-get install git cmake vim swig i2c-tools libi2c-dev ntpdate minicom python3.4-dev --assume-yes
 
 # Initial setup of i2c for the Chronodot, remapping of the UART on Pi3
 cat << EOF >> /boot/config.txt
