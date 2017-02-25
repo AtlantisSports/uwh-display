fbf82b1e0ef2660774ea2ea530a5a9feafcbf9e1
Jonathan Roelofs <jonathan@codesourcery.com>
2017-02-25 15:58:45 -0700

    [display] Show Git Sha and Build Date on the status page

--- lib/display/Status.cpp
+++ lib/display/Status.cpp
@@ -15,18 +15,35 @@
 #include <algorithm>
 #include <cctype>
 #include <cstdlib>
+#include <cstring>
 
-const UWHDPixel HostnameColor = UWHDPixel(200, 0, 255);
-const UWHDPixel BGColor       = UWHDPixel(  0, 0, 0);
+const UWHDPixel StatusColor1 = UWHDPixel(200, 0, 255);
+const UWHDPixel StatusColor2 = UWHDPixel(255, 0, 200);
+const UWHDPixel BGColor      = UWHDPixel(  0, 0,   0);
+
+static const char *UWHDGitSha = UWHD_GIT_SHA;
+static const char *UWHDBuildDate = UWHD_BUILD_DATE;
 
 void renderStatus(UWHDCanvas *C) {
+  // Hostname
   if (FILE *FP = popen("/bin/hostname", "r")) {
     char Hostname[17];
     fgets(Hostname, sizeof(Hostname)-1, FP);
     std::transform(std::begin(Hostname), std::end(Hostname),
                    std::begin(Hostname), ::toupper);
-    BigNumber::printf(C, 0, 0, HostnameColor, &BGColor, "%s", Hostname);
+    BigNumber::printf(C, 0, 0, StatusColor1, &BGColor, "%s", Hostname);
     pclose(FP);
   } else
-    BigNumber::printf(C, 0, 0, HostnameColor, &BGColor, "*ERR*");
+    BigNumber::printf(C, 0, 0, StatusColor1, &BGColor, "*ERR*");
+
+  // Version
+  char ShortSha[8];
+  memset(ShortSha, 0, sizeof(ShortSha));
+  strncpy(ShortSha, UWHDGitSha, sizeof(ShortSha)-1);
+  std::transform(std::begin(ShortSha), std::end(ShortSha),
+                 std::begin(ShortSha), ::toupper);
+  BigNumber::printf(C, 0, 8, StatusColor2, &BGColor, "UWHDD: %s", ShortSha);
+
+  // Build Date
+  BigNumber::printf(C, 0, 16, StatusColor1, &BGColor, "%s", UWHDBuildDate);
 }
